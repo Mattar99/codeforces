33990a663df0f52e4bb7e51bd27f1b439e8f774d
Ahmed Mattar <acematar@gmail.com>
2017-12-14 21:45:43 +0200

    Dominopiling50A

---
+++ A. Domino piling-/src/DominoPiling50A.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class DominoPiling50A {
+
+    public static void main(String[] args) {
+        byte M; Byte N;
+        Scanner sc = new Scanner(System.in);
+        M=sc.nextByte(); N=sc.nextByte();
+        System.out.println((M*N)/2);
+    }
+}
